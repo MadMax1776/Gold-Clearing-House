48859deb686096f937edf81629288b00e83abb74
mmeacham <maxwmeacham@gmail.com>
2020-04-22 22:07:38 -0400

    testing and learning python stuff

--- test.py
+++ test.py
@@ -1,6 +1,65 @@
 # Print welcome message
-print('hello world')
-
-world = 'roll tide'
-
-print(world)
+# print('hello world')
+#
+# world = 'roll tide'
+#
+# print(world)
+# =================================================
+# Note: change this value for a different result
+# num = 8
+#
+# # To take the input from the user
+# #num = float(input('Enter a number: '))
+#
+# num_sqrt = num ** 0.5
+# print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+# # ==============================================
+# x = 5
+# y = 10
+#
+# # To take inputs from the user
+# #x = input('Enter value of x: ')
+# #y = input('Enter value of y: ')
+# ===================================================
+# # create a temporary variable and swap the values
+# temp = x
+# x = y
+# y = temp
+#
+# print('The value of x after swapping: {}'.format(x))
+# print('The value of y after swapping: {}'.format(y))
+#
+# # importing the random module
+# import random
+#
+# print(random.randint(0,9))
+# # ============================================
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("{0} is Even".format(num))
+# else:
+#    print("{0} is Odd".format(num))
+## =============================
+# Program to check if a number is prime or not
+#
+# num = 407
+#
+# # To take input from the user
+# #num = int(input("Enter a number: "))
+#
+# # prime numbers are greater than 1
+# if num > 1:
+#    # check for factors
+#    for i in range(2,num):
+#        if (num % i) == 0:
+#            print(num,"is not a prime number")
+#            print(i,"times",num//i,"is",num)
+#            break
+#    else:
+#        print(num,"is a prime number")
+#
+# # if input number is less than
+# # or equal to 1, it is not prime
+# else:
+#    print(num,"is not a prime number")
+# ========================================
